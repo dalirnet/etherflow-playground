5f1a5da9a3fca29d5702d287eb703240ab7e5103
Amir Reza Dalir <dalirnet@gmail.com>
2021-12-23 19:37:26 +0330

    Fix typo

--- components/EtherflowLogo.vue
+++ components/EtherflowLogo.vue
@@ -2,6 +2,6 @@
     <nuxt-link class="flex justify-center items-center pt-8 sm:pt-0" to="/">
         <img src="/icon.png" width="32" height="32" />
         <strong class="text-3xl px-1">EtherFlow</strong>
-        <small class="pl-1 pt-2">playgraund</small>
+        <small class="pl-1 pt-2">playground</small>
     </nuxt-link>
 </template>
